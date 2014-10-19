18a81f333f1a6bb0860d2644e4af8ab0946aef2b
ST <ST@STs-MacBook-Pro-4.local>
2014-10-18 18:27:51 -0700

    First day with OpenCV. Facial Recognition works. Changed -1 to 0 to open webcam stream.

---
+++ OpenCV.cpp
@@ -0,0 +1,172 @@
+//
+//  main.cpp
+//  OpenCV Test
+//
+//  Created by ST on 10/18/14.
+//  Copyright (c) 2014 ST. All rights reserved.
+//
+
+
+#include <opencv2\opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+/** Function Headers */
+void detectAndDisplay( Mat frame );
+
+/** Global variables */
+String face_cascade_name = "C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";
+String eyes_cascade_name = "C:/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+CascadeClassifier face_cascade;
+CascadeClassifier eyes_cascade;
+String window_name = "Capture - Face detection";
+
+/** @function main */
+int main( void )
+{
+    VideoCapture capture;
+    Mat frame;
+
+    //-- 1. Load the cascades
+    if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading eyes cascade\n"); return -1; };
+
+    //-- 2. Read the video stream
+    capture.open( 0 );
+    if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    while ( capture.read(frame) )
+    {
+        if( frame.empty() )
+        {
+            printf(" --(!) No captured frame -- Break!");
+            break;
+        }
+
+        //-- 3. Apply the classifier to the frame
+        detectAndDisplay( frame );
+
+        int c = waitKey(10);
+        if( (char)c == 27 ) { break; } // escape
+    }
+    return 0;
+}
+
+/** @function detectAndDisplay */
+void detectAndDisplay( Mat frame )
+{
+    std::vector<Rect> faces;
+    Mat frame_gray;
+
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
+    equalizeHist( frame_gray, frame_gray );
+
+    //-- Detect faces
+    face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+        Mat faceROI = frame_gray( faces[i] );
+        std::vector<Rect> eyes;
+
+        //-- In each face, detect eyes
+        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+        for ( size_t j = 0; j < eyes.size(); j++ )
+        {
+            Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
+            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+            circle( frame, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+        }
+    }
+    //-- Show what you got
+    imshow( window_name, frame );
+}
+
+
+
+
+
+
+
+
+//#include <opencv2\opencv.hpp>
+//#include <iostream>
+
+/*
+using namespace cv;
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+	Mat image;
+
+	VideoCapture cap;
+
+	cap.open(0);
+
+	namedWindow("window",1);
+
+
+
+	while(1)
+
+	{
+
+		cap>>image;
+
+		if (!image.empty())
+			imshow("window",image);
+
+		waitKey(33);
+
+	}
+
+}
+*/
+
+/*
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+
+using namespace cv;
+
+using namespace std;
+
+int main(int argc, char** argv)
+
+{
+
+  Mat im = imread(argc == 2 ? argv[1] : "Lenna.png", 1);
+
+  if (im.empty())
+
+  {
+
+    cout << "Cannot open image!" << endl;
+
+    return -1;
+
+  }
+
+  imshow("image", im);
+
+  waitKey(0);
+
+  return 0;
+
+}
+
+*/
