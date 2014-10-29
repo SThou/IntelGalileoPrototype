8b3460f719474feaf9d41a19524630a0a791fd96
ST <Sthou3@gmail.com>
2014-10-29 00:34:40 -0700

    Rewrote the motor control to work on an H-Bridge driver L293D. Counter clockwise and clockwise directions are now possible

--- OpenCVMotorControl/OpenCVMotorControl.ino
+++ OpenCVMotorControl/OpenCVMotorControl.ino
@@ -2,21 +2,45 @@
 Motor control file for Intel Galileo 
 Pins available for analogWrite (3, 5 , 6 10, 11)
 
-Last update: Tuesday October 28, 2014 11:13 PM ST
+set true value to clockwise & false to counter clockwise
+
+Last update: Tuesday October 29, 2014 12:29 PM ST
 */
 
-int motorPin = 7;
- 
+
+int enablePin = 6; 
+int motorPin1 = 3;
+int motorPin2 = 5;
+
+
 void setup() 
 { 
-  pinMode(motorPin, OUTPUT); // change GPIO to output
+  pinMode(motorPin1, OUTPUT); // change GPIO to output
+  pinMode(motorPin2, OUTPUT); // change GPIO to output
+  pinMode(enablePin, OUTPUT); // change GPIO to output
 } 
  
  
 void loop() 
 { 
+      
 
-      analogWrite(motorPin, -100); // half speed
+    while(1){
+    setMotor(50, true);
+   // delay(1000);
+    //setMotor(100,0);
+    
+    }
+    
     
 
 } 
+
+
+void setMotor(int speeds, boolean reversedir)
+{
+  analogWrite(enablePin, speeds);
+  digitalWrite(motorPin1, !reversedir);
+  digitalWrite(motorPin2, reversedir);
+  
+}
